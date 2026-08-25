1cd43d034a967cc34e25fa2d5d41c04b717c81f5
ACT-STUDENT <ACT-STUDENT@LAB2-PC17>
2026-08-25 14:28:18 +0800

    started view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Denoy_INFASS2.Models;
+using Denoy_INFASS2.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Diagnostics;
@@ -52,6 +53,12 @@ namespace Denoy_INFASS2.Controllers
             return View();
         }
 
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
         [HttpPost]
         [Route("GetUser")]
         public IActionResult GetUser(
@@ -84,6 +91,41 @@ namespace Denoy_INFASS2.Controllers
             command.ExecuteNonQuery();
 
             return Content("User registered successfully!");
+        }
+
+        [HttpPost]
+        [Route("View")]
+        public IActionResult View()
+        {
+            var UserList = new List<UsersViewModel>();
+
+            string connectionString =
+                _configuration.GetConnectionString("DefaultConnection");
+
+            string sql = @"
+                SELECT * FROM Users";
+
+            using SqlConnection connection =
+                new SqlConnection(connectionString);
+
+            using SqlCommand command =
+                new SqlCommand(sql, connection);
+            connection.Open();
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var user = new UsersViewModel
+                {
+                    Username = reader["Username"] != DBNull.Value ? reader["Username"].ToString() : string.Empty,
+                    Email = reader["Email"] != DBNull.Value ? reader["Email"].ToString() : string.Empty,
+                    Password = reader["Password"] != DBNull.Value ? reader["Password"].ToString() : string.Empty
+                };
+
+                UserList.Add(user);
+            }   
+
+            return View(UserList);
         }
     }
 }
---
+++ ViewModels/UsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Denoy_INFASS2.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
